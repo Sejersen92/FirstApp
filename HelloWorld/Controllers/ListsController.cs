1978ba6420ad6ea0349ead64c57be2d0a179e848
Mikkel mise. Sejersen <mise@pc-tec.local>
2017-08-28 14:51:43 +0200

    yay tæst
    fisk

---
+++ HelloWorld/Controllers/ListsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class ListsController : Controller
+    {
+        private ListDBContext db = new ListDBContext();
+
+        // GET: Lists
+        public ActionResult Index(string Genre, string searchString)
+        {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Lists
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.Genre = new SelectList(GenreLst);
+
+            var list = from m in db.Lists
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                list = list.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(Genre))
+            {
+                list = list.Where(x => x.Genre == Genre);
+            }
+
+            return View(list);
+        }
+
+        // GET: Lists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lists list = db.Lists.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // GET: Lists/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Lists list)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lists.Add(list);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(list);
+        }
+
+        // GET: Lists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lists list = db.Lists.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // POST: Lists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Lists list)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(list).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Noget fejlede: "+e.Message);
+            }
+                    return View(list);
+        }
+
+        // GET: Lists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lists list = db.Lists.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // POST: Lists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lists list = db.Lists.Find(id);
+            db.Lists.Remove(list);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HelloWorld/Models/List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class Lists
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get => price; set => price = decimal.Parse(value.ToString()); }
+
+        private decimal price;
+    }
+
+    public class ListDBContext : DbContext
+    {
+        public DbSet<Lists> Lists { get; set; }
+    }
+}
